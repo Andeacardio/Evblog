16838ecab624355aff7570a99be488b3c75619cc
Andreacardio <Andrea.acardio@gmail.com>
2025-01-18 10:04:06 +0100

    ui/ux changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,44 +4,57 @@ import { Link } from "react-router";
 import "../../common.scss";
 
 const Header = () => {
+  function BurgerMenu() {
+    document.getElementById("links").classList.toggle("visible");
+    document.getElementById("buttons").classList.toggle("visible");
+    document.getElementById("header").classList.toggle("height");
+    document.getElementById("menu").classList.toggle("hidden");
+    document.getElementById("menu").classList.toggle("rotate");
+    document.getElementById("close").classList.toggle("visible");
+    document.getElementById("close").classList.toggle("rotate");
+  }
   return (
     <>
-      <div className="header">
-        <div className="container">
-          <div className="logo">
-            <a href="/">
-              <img src={Logo} alt="logo" />
-            </a>
-          </div>
-          <div className="links">
-            <Link className="link cLink" to="/?cat=art">
-              <h6>ART</h6>
+      <div className="header" id="header">
+        <div className="logo">
+          <a href="/">
+            <img src={Logo} alt="logo" />
+          </a>
+          <button id="close" onClick={() => BurgerMenu()}>
+            X
+          </button>
+          <button id="menu" onClick={() => BurgerMenu()}>
+            III
+          </button>
+        </div>
+        <div className="links" id="links">
+          <Link className="link cLink" to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className="link cLink" to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link cLink" to="/?cat=tecnology">
+            <h6>TECNOLOGY</h6>
+          </Link>
+          <Link className="link cLink" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link cLink" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link cLink" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+        </div>
+        <div className="buttons" id="buttons">
+          <span>John</span>
+          <span>Log out</span>
+          <span className="write">
+            <Link className="cLink" to="/write">
+              Write
             </Link>
-            <Link className="link cLink" to="/?cat=science">
-              <h6>SCIENCE</h6>
-            </Link>
-            <Link className="link cLink" to="/?cat=tecnology">
-              <h6>TECNOLOGY</h6>
-            </Link>
-            <Link className="link cLink" to="/?cat=cinema">
-              <h6>CINEMA</h6>
-            </Link>
-            <Link className="link cLink" to="/?cat=design">
-              <h6>DESIGN</h6>
-            </Link>
-            <Link className="link cLink" to="/?cat=food">
-              <h6>FOOD</h6>
-            </Link>
-          </div>
-          <div className="buttons">
-            <span>John</span>
-            <span>Log out</span>
-            <span className="write">
-              <Link className="cLink" to="/write">
-                Write
-              </Link>
-            </span>
-          </div>
+          </span>
         </div>
       </div>
     </>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,26 @@ import { Link } from "react-router";
 import posts from "../../data";
 import "./Home.scss";
 import "../../common.scss";
+import Img from "../../assets/img/1.jpg";
 
 const Home = () => {
   return (
     <>
       <div className="home">
+        <div className="top">
+          <div className="picture">
+            <div className="img">
+              <img src={Img} alt="" />
+            </div>
+          </div>
+          <div className="welcome">
+            <h1>Welcome to EvBlob!</h1>
+            <p>
+              This is a place where you can share your opinions and experince
+              about different topics.
+            </p>
+          </div>
+        </div>
         <div className="posts">
           {posts.map((post) => {
             return (
@@ -16,7 +31,7 @@ const Home = () => {
                 </div>
                 <div className="content">
                   <Link to={`/post/${post.id}`} className="link cLink">
-                    <h1>{post.title}</h1>
+                    <h2>{post.title}</h2>
                   </Link>
                   <p>{post.desc}</p>
                   <button>Read more</button>
